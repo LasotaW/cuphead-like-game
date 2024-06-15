122dcecc9b4d589fb35467d01a676b57b120f124
Wiktor Lasota <37120203+LasotaW@users.noreply.github.com>
2024-06-15 21:37:39 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -14,36 +14,98 @@ path = os.path.join(os.getcwd(), 'images')
 
 class Level:
     def __init__(self):
+        self.scroll = 0
         self.bg_images = []
         for i in range(1, 6):
-            self.bg_images.append(pg.image.load(path + f"\\background\{i}.png").convert_alpha())
+            self.bg_images.append(pg.image.load(path + f"\\background\\{i}.png").convert_alpha())
 
     def draw_bg(self):
         bg_width = self.bg_images[0].get_width()
-        for x in range(10):
+        for x in range(3):
             speed = 1
             for i in self.bg_images:
-                screen.blit(i, (x * bg_width - scroll * speed, 0))
+                if self.scroll <= 0:
+                     self.scroll = 0
+                screen.blit(i, (x * bg_width - self.scroll * speed, 0))
                 speed += 0.3
-    def draw_ground():
+                   
+
+    def draw_ground(self):
         pass
 
+    def update(self):
+        self.draw_bg()
+        key = pg.key.get_pressed()
+        if key[pg.K_LEFT]:
+            self.scroll -= 5
+        if key[pg.K_RIGHT]:
+            self.scroll += 5
+
+
 class Player(pg.sprite.Sprite):
-    def __init__(self, image, cx, cy):
+    def __init__(self, images, state, x ,y):
         super().__init__()
-        self.image = image
-        self.image
+        self.x = x
+        self.y = y
+        self.images = images
+        self.state = state
+        self.isJump = False
+        self.jumpCount = 10
+        self.current_sprite = 0
+        self.health = 100
+        self.animations = {
+            'idle': self._load_images(self.images + 'idle', 2),
+            'run': self._load_images(self.images + 'run', 2),
+            'shoot': self._load_images(self.images + 'shoot', 2),
+            'jump': self._load_images(self.images + 'jump', 1)
+        }
+        self.image = self.animations[self.state][self.current_sprite]
         self.rect = self.image.get_rect()
-        self.rect.center = cx, cy
+        self.rect.center = self.x, self.y
 
-    def draw_player(self, surface):
-        surface.blit(self.image, self.rect)
+    def _load_images(self, action, num_frames):
+        images = []
+        for i in range(1, num_frames+1):
+            img = pg.image.load(f"{action}{i}.png").convert_alpha()
+            images.append(img)
+        return images
 
-player_img = pg.image.load(path + "\player\player.png").convert_alpha()
-print(path + "\player.png")
-player = Player(player_img, 100, 500)
+    def update(self):
+        self.current_sprite += 0.3
+        
+        key = pg.key.get_pressed()
+        if key[pg.K_SPACE]:
+            self.state = 'shoot'
+        elif key[pg.K_UP] and not self.isJump:
+            
+            self.state = 'jump'
+        elif key[pg.K_RIGHT]:
+            self.state = 'run'
+        elif key[pg.K_LEFT]:
+            self.state = 'run'
+        else:
+            self.state = 'idle'
 
-scroll = 0
+        if self.current_sprite >= len(self.animations[self.state]):
+            self.current_sprite = 0
+        
+        self.image = self.animations[self.state][int(self.current_sprite)]
+
+
+class Bullet:
+    pass
+
+class Enemy:
+    pass
+
+class Boss(Enemy):
+    pass
+
+player = Player(path + "\\player\\", 'idle', 100, 500)
+l1 = Level()
+moving_sprites = pg.sprite.Group()
+moving_sprites.add(player)
+
 run = True
 while run:
     for event in pg.event.get():
@@ -51,17 +113,13 @@ while run:
             run = False
         
     #Render level
-    l1 = Level()
-    l1.draw_bg()
-    key = pg.key.get_pressed()
-    if key[pg.K_LEFT]:
-        scroll -= 5
-    if key[pg.K_RIGHT]:
-        scroll += 5
+   
+    l1.update()
 
     #Draw player
-    player.draw_player(screen)
-
+    player.update()
+    #window update
+    moving_sprites.draw(screen)
     clock.tick(FPS)
     pg.display.flip()
 
